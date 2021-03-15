6cc2cb66b57cd42409241b2621097be1f60353f4
gkarpov-de <gkarpov@icloud.com>
2021-03-15 19:35:57 +0100

    initial commit

---
+++ src/test/java/org/jacoco/examples/maven/java/HelloWorldTest.java
@@ -0,0 +1,22 @@
+package org.jacoco.examples.maven.java;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+
+    private HelloWorld subject;
+
+    @BeforeEach
+    public void setup() {
+        subject = new HelloWorld();
+    }
+
+    @Test
+    public void testGetMessage() {
+        assertEquals("Hello World!", subject.getMessage(false));
+    }
+
+}
